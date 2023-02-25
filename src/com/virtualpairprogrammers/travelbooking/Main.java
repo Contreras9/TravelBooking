1b5fee3baed3acfadf19a09daec06d007d7026dd
Hamzah Contreras <h.contreras9@outlook.com>
2023-02-25 01:09:29 -0600

    OO-Part1-Inheritance/Inheriting a class

---
+++ src/com/virtualpairprogrammers/travelbooking/Main.java
@@ -0,0 +1,12 @@
+package com.virtualpairprogrammers.travelbooking;
+
+import com.virtualpairprogrammers.travelbooking.domain.TrainTicket;
+import com.virtualpairprogrammers.travelbooking.domain.TravelTicket;
+
+public class Main {
+    public static void main(String[] args) {
+        TrainTicket trainTicket = new TrainTicket();
+        trainTicket.cancel();
+
+    }
+}
---
+++ src/com/virtualpairprogrammers/travelbooking/domain/TrainTicket.java
@@ -0,0 +1,32 @@
+package com.virtualpairprogrammers.travelbooking.domain;
+
+public class TrainTicket extends TravelTicket{
+
+    private Integer travelClass;
+    private Integer cartNumber;
+    private Integer seatNumber;
+
+    public Integer getTravelClass() {
+        return travelClass;
+    }
+
+    public void setTravelClass(Integer travelClass) {
+        this.travelClass = travelClass;
+    }
+
+    public Integer getCartNumber() {
+        return cartNumber;
+    }
+
+    public void setCartNumber(Integer cartNumber) {
+        this.cartNumber = cartNumber;
+    }
+
+    public Integer getSeatNumber() {
+        return seatNumber;
+    }
+
+    public void setSeatNumber(Integer seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+}
