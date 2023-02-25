e8a3e3316a9dfc66b39cfd73630ec0f2e83b55b2
Hamzah Contreras <h.contreras9@outlook.com>
2023-02-25 12:52:24 -0600

    OO-Part1-Inheritance/Overriding methods

--- src/com/virtualpairprogrammers/travelbooking/Main.java
+++ src/com/virtualpairprogrammers/travelbooking/Main.java
@@ -1,5 +1,6 @@
 package com.virtualpairprogrammers.travelbooking;
 
+import com.virtualpairprogrammers.travelbooking.domain.BusTicket;
 import com.virtualpairprogrammers.travelbooking.domain.TrainTicket;
 
 import java.math.BigDecimal;
@@ -10,10 +11,13 @@ public class Main {
         TrainTicket trainTicket = new TrainTicket();
         trainTicket.cancel();
 
-        TrainTicket trainTicket2 = new TrainTicket(123L, "London", "Edinburgh", new BigDecimal("59.00"),
-                LocalDateTime.of(2022, 3, 7, 16, 3),
-                LocalDateTime.of(2022, 3, 7, 19, 3), 1, 3, 45);
-
+        TrainTicket trainTicket2 = new TrainTicket(123L,"London","Edinburgh", new BigDecimal("59.00"), LocalDateTime.of(2022,9,30,16,3), LocalDateTime.of(2023,3,7,19,3),1,3,45);
         trainTicket2.upgrade();
+
+        String[] providers = {"1st bus company", "2nd bus company"};
+        BusTicket busTicket = new BusTicket(125L,"London","Edinburgh", new BigDecimal("59.00"), LocalDateTime.of(2023,3,7,16,3), LocalDateTime.of(2023,3,7,19,3), providers);
+        trainTicket2.cancel();
+        busTicket.cancel();
+
     }
 }
