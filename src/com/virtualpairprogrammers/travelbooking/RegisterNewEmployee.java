f0390407d0c3dbf62f494d5f362f3e0bef20969b
Hamzah Contreras <h.contreras9@outlook.com>
2023-02-26 17:25:31 -0600

    Console I/O-Reading input from the console

---
+++ src/com/virtualpairprogrammers/travelbooking/RegisterNewEmployee.java
@@ -0,0 +1,18 @@
+package com.virtualpairprogrammers.travelbooking;
+
+import java.util.Scanner;
+
+public class RegisterNewEmployee {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        Integer i = scanner.nextInt();
+        scanner.nextLine();
+        String s2 = scanner.nextLine();
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(s2);
+    }
+}
