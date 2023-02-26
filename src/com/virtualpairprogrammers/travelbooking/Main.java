caf246cddf36568020afac968f6af7fa57da9b1a
Hamzah Contreras <h.contreras9@outlook.com>
2023-02-26 02:35:04 -0600

    Enums-Practice

--- src/com/virtualpairprogrammers/travelbooking/Main.java
+++ src/com/virtualpairprogrammers/travelbooking/Main.java
@@ -2,6 +2,7 @@ package com.virtualpairprogrammers.travelbooking;
 
 import com.virtualpairprogrammers.travelbooking.domain.BusTicket;
 import com.virtualpairprogrammers.travelbooking.domain.TrainTicket;
+import com.virtualpairprogrammers.travelbooking.domain.TravelClass;
 import com.virtualpairprogrammers.travelbooking.domain.TravelTicket;
 
 import java.math.BigDecimal;
@@ -12,7 +13,7 @@ public class Main {
         TrainTicket trainTicket = new TrainTicket();
         trainTicket.cancel();
 
-        TrainTicket trainTicket2 = new TrainTicket(123L,"London","Edinburgh", new BigDecimal("59.00"), LocalDateTime.of(2022,9,30,16,3), LocalDateTime.of(2023,3,7,19,3),1,3,45);
+        TrainTicket trainTicket2 = new TrainTicket(123L,"London","Edinburgh", new BigDecimal("59.00"), LocalDateTime.of(2022,9,30,16,3), LocalDateTime.of(2023,3,7,19,3), TravelClass.FIRST,3,45);
         trainTicket2.upgrade();
 
         String[] providers = {"1st bus company", "2nd bus company"};
